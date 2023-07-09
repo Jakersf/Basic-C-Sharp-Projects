fdb21b9440b4d9877247100abd6cb1544514d818
Jakersf <ecxzero1@gmail.com>
2023-07-08 20:59:57 -0500

    main method submission

---
+++ Basic_C#_Programs/MainMethod/MainMethod/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Math
+    {
+        public int Double(int num) // method that returns the number the method is performed on times 2
+        {
+            return num * 2; 
+        }
+        public int Add4(decimal num) // method that returns the number the method is performed on plus 4
+        {
+            return Convert.ToInt32(num + 4); //This method takes in a decimal and converts it to an integer
+        }
+        public int Subtract5(string numString) // method that returns the number the method is performed on minus 5
+        {
+            
+            int result = Convert.ToInt32(numString) - 5; //This method converts the integer equatable string to an integer and uses it in the calculations
+            return result;
+
+        }
+        
+
+    }
+}
---
+++ Basic_C#_Programs/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Math math = new Math(); 
+
+            int num1 = 5;
+            int total = math.Double(num1); //Performs the "Double" method on the variable num1
+
+            decimal num2 = 10;
+            int total2 = math.Add4(num2); //Performs the "Add4" method on the variable num2
+
+            string num3 = "20";
+            int total3 = math.Subtract5(num3); //Perfroms the "Subtract5" method on the variable num2
+            
+            
+
+
+            Console.WriteLine(num1 + " times 2 = " + total); //Displays the results of the Double method
+            Console.WriteLine(num2 + " plus 4 = " + total2); //Displays the results of the Add4 method
+            Console.WriteLine(num3 + " minus 5 = " + total3); //Displays the results of the Subtract5 method
+           
+            Console.ReadLine();
+
+        }
+    }
+}
