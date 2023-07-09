4b7bbf167de2cd07177c83ce0deab5a3150eacbe
Jakersf <ecxzero1@gmail.com>
2023-07-08 23:54:16 -0500

    method

---
+++ Basic_C#_Programs/MethodClass/MethodClass/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Math
+    {
+        public void quickmaths(int num1, int num2) //Creates a method that performs a math operation on the first number, and displays the second one. 
+            {
+
+                Console.WriteLine(num2); //Displays integer 2
+                int result = num1 + 2; //Performs a math operation on integer 1
+
+            }
+    }
+}
---
+++ Basic_C#_Programs/MethodClass/MethodClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 5; //Declares 2 numbers to pass into the method
+            int num2 = 4;
+
+
+            Math math = new Math(); //Instantiates the method
+            math.quickmaths(num1, num2); //Calls the method and passes it two numbers
+
+            Console.ReadLine(); 
+
+            
+        }
+    }
+}
