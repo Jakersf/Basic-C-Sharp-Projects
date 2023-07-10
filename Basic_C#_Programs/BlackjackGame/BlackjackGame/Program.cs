d61e17d7aecfa3edb73085b8b46eec1e78fa21d9
Jakersf <ecxzero1@gmail.com>
2023-07-10 01:24:38 -0500

    ParsingEnums

--- Basic_C#_Programs/BlackjackGame/BlackjackGame/Program.cs
+++ Basic_C#_Programs/BlackjackGame/BlackjackGame/Program.cs
@@ -11,7 +11,7 @@ namespace BlackjackGame
         static void Main(string[] args)
         {
             
-
+            
             Deck deck = new Deck();
             deck.Shuffle(3);
 
---
+++ Basic_C#_Programs/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Please enter the current day of the week"); //Asks the user to enter a day of the week
+
+                DaysOfTheWeek Input = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine()); //Creates a variable named input, then uses Enum.Parse to convert the user's input to the Enum DaysOfTheWeek data type
+                
+                Console.WriteLine(Input); //Writes the inputted day of the week
+            }
+
+            catch (ArgumentException) //Catch blocks catch errors that commonly occur due to user error
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+
+
+        public enum DaysOfTheWeek //Creates an Enum called DaysOfTheWeek and assigns it a value equivalent to each day of the week.
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
