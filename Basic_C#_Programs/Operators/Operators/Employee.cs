c6ecc65907ef1c7f0d36eb30fbb5a80ef6439f87
Jakersf <ecxzero1@gmail.com>
2023-07-09 23:40:04 -0500

    Operators

---
+++ Basic_C#_Programs/Operators/Operators/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee //Creates a public class called employee
+    {
+        public string FirstName { get; set; } //These 3 properties are the FirstName, LastName, and ID properties.
+        public string LastName { get; set; }
+        public int ID { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2) //Overloads the == operator and compares the first inputted employee's id and the second one's id to see if they are equal.
+        {
+            return employee1.ID == employee2.ID;
+
+        }
+        public static bool operator!= (Employee employee1, Employee employee2) //Overloads the == operator and compares the first inputted employee's id and the second one's id to see if they are not equal.
+        {
+            return employee1.ID != employee2.ID;
+        }
+    }
+
+}
---
+++ Basic_C#_Programs/Operators/Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee1 = new Employee() { FirstName = "John", LastName = "Doe", ID = 1 }; //Creates 2 employee objects using the employee class.
+            Employee employee2 = new Employee() { FirstName = "John", LastName = "Smith", ID = 2 };
+
+            Console.WriteLine(employee1 == employee2); //Uses the overloaded == operator to compare employee1 and employee2, then writes the result to the console.
+            Console.ReadLine();
+           
+        }
+    }
+}
